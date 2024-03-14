e47488a59e846855bfc35811634ba7b0e126e4e1
tokisaki9909 <ifoster615@gmail.com>
2024-03-14 15:34:14 +0800

    BINGO

--- Index.js
+++ Index.js
@@ -1,18 +1,21 @@
 console.log("Hello World.");
 
-function generateRandomNumber () {
+generateRandomNumber(15);
+
+function generateRandomNumber (max) {
     // return random number 0 to 10;
-    let randomNum = Math.floor(Math.random() * 15) + 1;
+    let randomNum = Math.floor(Math.random() * max) + 1;
 
     // if...eles if... else statement
     //if (condition) {block to execute}
     //==, ! =, >, <, >=, < =, &&, ||
 
-    if(randomNum >= 5){
-       console.log(`Random Number is > = to 5`);
+    if(randomNum <= 15) {
+      return console.log(`Random Number $(randomNum) is in B. 1 - 15`);
+    } else if (randomNum > 16 && randomNum <  30) {
+      return console.log(`Random Number $(randomNum) is in I. 16 - 30`);
     } else {
-       console.log(`Random Number is < = to 5`);
+      return console.log(`Random Number is invalid.`);
     }
-
-    return console.log(randomNum);
-}
+ }
+    
