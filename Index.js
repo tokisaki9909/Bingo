06f8a15c4064b8b94f70606f6362b0d0261c7827
tokisaki9909 <ifoster615@gmail.com>
2024-03-14 15:46:31 +0800

    BingOOO

--- Index.js
+++ Index.js
@@ -12,10 +12,22 @@ function generateRandomNumber (max) {
 
     if(randomNum <= 15) {
       return console.log(`Random Number $(randomNum) is in B. 1 - 15`);
-    } else if (randomNum > 16 && randomNum <  30) {
+    } 
+    else if (randomNum > 16 && randomNum <  30) {
       return console.log(`Random Number $(randomNum) is in I. 16 - 30`);
-    } else {
+    }
+    else if (randomNum > 31 && randomNum <  45) {
+      return console.log(`Random Number $(randomNum) is in N. 31 - 45`);
+    }
+    else if (randomNum > 46 && randomNum <  60) {
+      return console.log(`Random Number $(randomNum) is in G. 46 - 60`);
+    }
+    else if (randomNum > 61 && randomNum <  75) {
+      return console.log(`Random Number $(randomNum) is in O. 61 - 75`);
+    }
+
+     else {
       return console.log(`Random Number is invalid.`);
     }
- }
     
+ }
